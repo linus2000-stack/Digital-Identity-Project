cb06c9f3903111394b28eac3212387f85cd49269
teohbenji <teohbenjamin1@gmail.com>
2024-06-19 15:21:48 +0800

    Init rspec-rails test case

---
+++ spec/models/user_particulars_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe UserParticulars, type: :model do
+  # Seed the database before running any tests
+  before(:all) do
+    Rails.application.load_seed
+  end
+
+  # Rollback transaction after each test case
+  around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+
+  describe '.first_user_particulars' do
+    it 'returns the first user particulars in the database' do
+      user = UserParticulars.first_user_particulars
+      expect(user).to have_attributes(full_name: 'John Doe', phone_number: '123-456-7890')
+    end
+  end
+
+  describe '.last_user_particulars' do
+    it 'returns the last user particulars in the database' do
+      user = UserParticulars.last_user_particulars
+      expect(user).to have_attributes(full_name: 'Emma Davis', phone_number: '444-444-4444')
+    end
+  end
+end
