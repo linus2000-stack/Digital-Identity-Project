2fe9367c46288ca618d96d5bf6e887dbc58e690b
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-20 01:47:55 +0800

    Midtest
    Views

--- app/controllers/user_particulars_controller.rb
+++ app/controllers/user_particulars_controller.rb
@@ -2,6 +2,9 @@ class UserParticularsController < ApplicationController
   def show
     # Retrieve the user particular by ID
     @user_particular = UserParticular.find_by_id(params[:id])
+    if @user_particular.nil?
+        flash['alert'] = "User cannot be found!"
+    end
   end
   
 
@@ -11,7 +14,7 @@ class UserParticularsController < ApplicationController
     #TODO: Parse date format before passing to model???
 
     #id retrieve from the returned model
-    @user_particular = UserParticular.create_user_particular(user_particular_params)
+    @user_particular = UserParticular.create_user_particular(params.permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin, :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival))
     
     #redirect to show
     redirect_to user_particular_path(@user_particular.id)
@@ -22,6 +25,7 @@ class UserParticularsController < ApplicationController
   end
 
   def confirm
+    @user_particulars = params.permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin, :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival, :birth_certificate, :passport, :other_identity_documents)
     #automatically renders app/views/user_particulars/confirm.html.erb
   end
 
