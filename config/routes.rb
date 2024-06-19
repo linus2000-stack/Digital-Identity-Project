d3e0c1adc5302c81ff17efd96936afa88887c7fa
teohbenji <teohbenjamin1@gmail.com>
2024-06-19 21:47:33 +0800

    Deleted unused homecontroller and corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'user_particulars#home' #set home path of web app as /user_particulars/home for now
   
-  #Resources: UserParticulars
-  resources :user_particulars
 
-  #Self-declared extra routes
+  #Self-declared extra routes (must be before resources)
   get 'user_particulars/confirm'
   get 'user_particulars/home'
 
+  #Resources: UserParticulars
+  resources :user_particulars
+  
 end
