3da8c5986d7e014f91d2eaa3b4ed6056d9d73815
teohbenji <teohbenjamin1@gmail.com>
2024-06-19 17:34:36 +0800

    Updated model and rspec for model

--- app/models/user_particular.rb
+++ app/models/user_particular.rb
@@ -1,9 +1,11 @@
-class UserParticular < ActiveRecord::Base
-    def self.first_user_particulars
-        first
+class UserParticular < ActiveRecord::Base    
+    def self.create_and_return_id(attributes)
+        user_particular = UserParticular.create(attributes)
+        user_particular.id
     end
 
-    def self.last_user_particulars
-        last
-    end    
+    def self.find_by_id(user_particular_id)
+        UserParticular.find_by(id: user_particular_id)
+    end
+      
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
 seed_user_particulars = [
-  { full_name: 'John Doe', phone_number: '123-456-7890' },
-  { full_name: 'Jane Smith', phone_number: '987-654-3210' },
-  { full_name: 'Alice Johnson', phone_number: '555-555-5555' },
-  { full_name: 'Bob Anderson', phone_number: '111-222-3333' },
-  { full_name: 'Emma Davis', phone_number: '444-444-4444' }
+  { 
+    full_name: 'John Doe', 
+    phone_number: '123-456-7890',
+    secondary_phone_number: '555-123-4567',
+    country_of_origin: 'United States',
+    ethnicity: 'Caucasian',
+    religion: 'Christian',
+    gender: 'Male',
+    date_of_birth: '1990-01-01',
+    date_of_arrival: '2020-01-01',
+    photo_url: 'https://example.com/john_doe_photo.jpg',
+    birth_certificate_url: 'https://example.com/john_doe_birth_certificate.jpg',
+    passport_url: 'https://example.com/john_doe_passport.jpg'
+  },
+  { 
+    full_name: 'Jane Smith', 
+    phone_number: '987-654-3210',
+    country_of_origin: 'Canada',
+    ethnicity: 'Asian',
+    religion: 'Buddhist',
+    gender: 'Female',
+    date_of_birth: '1995-03-15',
+    date_of_arrival: '2018-05-20',
+    photo_url: 'https://example.com/jane_smith_photo.jpg',
+    birth_certificate_url: 'https://example.com/jane_smith_birth_certificate.jpg',
+    passport_url: 'https://example.com/jane_smith_passport.jpg'
+  },
 ]
 
 seed_user_particulars.each do |user_particular|
