22fa04ada80df660fd3acbffdc3872337f599823
teohbenji <teohbenjamin1@gmail.com>
2024-06-19 21:19:06 +0800

    Create controller and views for UserParticulars

---
+++ app/controllers/user_particulars_controller.rb
@@ -0,0 +1,24 @@
+class UserParticularsController < ApplicationController
+  def show
+    #automatically renders app/views/user_particulars/show.html.erb
+  end
+
+  def create
+    #create new userparticulars in database, retrieve item of new row 
+
+    # calls show method, displays userparticulars of certain id
+    redirect_to user_particular_path(1)
+  end
+
+  def new
+    #automatically renders app/views/user_particulars/new.html.erb
+  end
+
+  def confirm
+    #automatically renders app/views/user_particulars/confirm.html.erb
+  end
+
+  def home
+    #automatically renders app/views/user_particulars/home.html.erb
+  end
+end
---
+++ spec/requests/user_particulars_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "UserParticulars", type: :request do
+  describe "GET /show" do
+    xit "returns http success" do
+      get "/user_particulars/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    xit "returns http success" do
+      get "/user_particulars/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    xit "returns http success" do
+      get "/user_particulars/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /confirm" do
+    xit "returns http success" do
+      get "/user_particulars/confirm"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
