783ccc59734695e4808486cb6a8d5cc73150246e
teohbenji <teohbenjamin1@gmail.com>
2024-06-19 21:19:06 +0800

    Create controller and views for UserParticulars

---
+++ app/helpers/user_particulars_helper.rb
@@ -0,0 +1,2 @@
+module UserParticularsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'user_particulars#home' #set home path of web app as /user_particulars/home for now
+  
+  #Resources: UserParticulars
+  resources :user_particulars
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
+  #Self-declared extra routes
+  get 'user_particulars/confirm'
+  get 'user_particulars/home'
+
 end
