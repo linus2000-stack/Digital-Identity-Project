00b5532a2d7ddf07b87807dbfee4451bbe8beb9e
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-20 15:40:23 +0800

    Minute changes, Cleaner Code
    Before working on session

--- app/controllers/user_particulars_controller.rb
+++ app/controllers/user_particulars_controller.rb
@@ -1,48 +1,43 @@
 class UserParticularsController < ApplicationController
   include UserParticularsHelper
-  def show
-    # Retrieve the user particular by ID
-    @user_particular = UserParticular.find_by_id(params[:id])
-  end
+
+  before_action :set_user_particular, only: [:show]
+  def show; end # No need for content when using @user_particular from before_action
   
-
   def create
-    #create new userparticulars in database, retrieve item of new row 
-    
-    #TODO: Parse date format before passing to model???
-
-    #id retrieve from the returned model
-    @user_particular = user_particular_params
-    @user_particular = UserParticular.create_user_particular(user_particular_params)
-    flash[:notice] = "Digital ID was successfully created!"
-    redirect_to user_particular_path(@user_particular.id)
+    @user_particular = UserParticular.new(user_particular_params) #Model object with keyed params
+    if @user_particular.save #the .save method checks whether the @user_particular was actually saved in the database.
+      flash[:notice] = "Digital ID was successfully created!"
+      redirect_to @user_particular
+    end
   end
 
   def new
-    @user_particular = UserParticular.new #create empty model instance to populate in form input!
-    @countries = country_options
-    @ethnicities = ethnicity_options
-    @religions = religion_options
-    #automatically renders app/views/user_particulars/new.html.erb    
+    @user_particular = UserParticular.new #An empty Model object to store the new details to be keyed in
+    set_dropdown_options # DRY up options population
   end
 
   def confirm
-    #TODO: Rename to singular naming
-    #@user_particular = params.permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin, :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival, :birth_certificate, :passport, :other_identity_documents)
-    @user_particular = UserParticular.new #create empty model instance to populate in form input INVISIBLE!    
-    @user_particulars = user_particular_params
-    session[:user] = @user_particulars
-    #automatically renders app/views/user_particulars/confirm.html.erb
+    @user_particular = UserParticular.new(user_particular_params) #The Model object to store the hidden keyed params
+    # Use the session to store the data (or a separate model) if necessary.
   end
 
-  def home
-    #automatically renders app/views/user_particulars/home.html.erb
-  end
+  def home; end
 
   private
 
   def user_particular_params
     params.require(:user_particular).permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin, :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival, :passport_photo, :birth_certificate, :passport, :other_identity_documents)
+  end
+
+  def set_user_particular
+    @user_particular = UserParticular.find_by_id(params[:id])
+  end
+
+  def set_dropdown_options
+    @countries = country_options
+    @ethnicities = ethnicity_options
+    @religions = religion_options
   end
 
 end
