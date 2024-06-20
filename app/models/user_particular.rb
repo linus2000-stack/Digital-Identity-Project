3088adff8e2672a5e71ad63b5d6149dbea79705b
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-20 14:21:22 +0800

    Fixed with Params Method
    Checkpoint!

--- app/models/user_particular.rb
+++ app/models/user_particular.rb
@@ -1,8 +1,7 @@
-class UserParticular < ActiveRecord::Base    
+class UserParticular < ActiveRecord::Base  
     def self.create_user_particular(attributes)
         UserParticular.create(attributes)
     end
-
     def self.find_by_id(id)
         UserParticular.find_by(id: id)
     end
