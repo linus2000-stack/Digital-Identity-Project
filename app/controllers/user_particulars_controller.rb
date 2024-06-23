46c7ec978d5b3e344b855607c4c9a1c665c217d0
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-24 04:04:05 +0800

    Beautified RSP
    Extension enabled beautify

--- app/controllers/user_particulars_controller.rb
+++ app/controllers/user_particulars_controller.rb
@@ -1,44 +1,47 @@
-class UserParticularsController < ApplicationController
-  include UserParticularsHelper
+# frozen_string_literal: true
 
-  before_action :set_user_particular, only: [:show]
-  def show; end # No need for content when using @user_particular from before_action
-  
-  def create
-    @user_particular = UserParticular.new(user_particular_params) #Model object with keyed params
-    if @user_particular.save #the .save method checks whether the @user_particular was actually saved in the database.
-      flash[:notice] = "Digital ID was successfully created!"
-      redirect_to @user_particular
-    end
-  end
+include UserParticularsHelperclass UserParticularsController < ApplicationController # For Model UserParticulars
 
-  def new
-    @user_particular = UserParticular.new(session.fetch(:user_particular_params, {}))
-    #An Model object based on session details
-    set_dropdown_options # Only populate options if it's a fresh visit to the page.
-  end
+before_action :set_user_particular, only: [:show]
+def show; end
+# No need for content when using @user_particular from before_action
 
-  def confirm
-    session[:user_particular_params] = user_particular_params # Use the session to store the model
-    @user_particular = UserParticular.new(user_particular_params) #The Model object to store the hidden keyed params
-  end
+def create
+  @user_particular = UserParticular.new(user_particular_params) # Model object with keyed params
+  # the .save method checks whether the @user_particular was actually saved in the database.
+  return unless @user_particular.save
 
-  def home; end
+  flash[:notice] = 'Digital ID was successfully created!'
+  redirect_to @user_particular
+end
+
+def new
+  @user_particular = UserParticular.new(session.fetch(:user_particular_params, {}))
+  # An Model object based on session details
+  set_dropdown_options # Only populate options if it's a fresh visit to the page.
+end
+
+def confirm
+  session[:user_particular_params] = user_particular_params # Use the session to store the model
+  @user_particular = UserParticular.new(user_particular_params) # The Model object to store the hidden keyed params
+end
+
+def home; end
 
   private
 
-  def user_particular_params
-    params.require(:user_particular).permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin, :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival, :passport_photo, :birth_certificate, :passport, :other_identity_documents)
-  end
+def user_particular_params
+  params.require(:user_particular).permit(:full_name, :phone_number, :secondary_phone_number, :country_of_origin,
+                                          :ethnicity, :religion, :gender, :date_of_birth, :date_of_arrival,
+                                          :passport_photo, :birth_certificate, :passport, :other_identity_documents)
+end
 
-  def set_user_particular
-    @user_particular = UserParticular.find_by_id(params[:id])
-  end
+def set_user_particular
+  @user_particular = UserParticular.find_by_id(params[:id])
+end
 
-  def set_dropdown_options
-    @countries = country_options
-    @ethnicities = ethnicity_options
-    @religions = religion_options
-  end
-
+def set_dropdown_options
+  @countries = country_options
+  @ethnicities = ethnicity_options
+  @religions = religion_options
 end
