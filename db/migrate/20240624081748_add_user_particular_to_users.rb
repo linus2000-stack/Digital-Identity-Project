b38cc4598f3635a57fb18c0018ccfcb8415e9083
teohbenji <teohbenjamin1@gmail.com>
2024-06-24 21:15:48 +0800

    Minor fix for migration

--- db/migrate/20240624081748_add_user_particular_to_users.rb
+++ db/migrate/20240624081748_add_user_particular_to_users.rb
@@ -1,5 +1,5 @@
 class AddUserParticularToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_reference :users, :profile, foreign_key: true
+    add_reference :users, :user_particulars, foreign_key: true
   end
 end
