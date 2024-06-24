902ac4e0cbf1d05cd5ea36e9cf60ca412db2db98
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-24 22:24:14 +0800

    Update user_particular.rb

--- app/models/user_particular.rb
+++ app/models/user_particular.rb
@@ -1,8 +1,9 @@
-class UserParticular < ActiveRecord::Base  
-    def self.create_user_particular(attributes)
-        UserParticular.create(attributes)
-    end
-    def self.find_by_id(id)
-        UserParticular.find_by(id: id)
-    end
+class UserParticular < ActiveRecord::Base
+  def self.create_user_particular(attributes)
+    UserParticular.create(attributes)
+  end
+
+  def self.find_by_id(id)
+    UserParticular.find_by(id:)
+  end
 end
