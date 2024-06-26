596e297b15172cc864413d31fa3be1191c421150
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-27 03:28:20 +0800

    Error and validation for registering, editing forget password with toggleable phone/email option

--- app/controllers/users/registrations/passwords_controller.rb
+++ app/controllers/users/registrations/passwords_controller.rb
@@ -1,15 +1,41 @@
 # frozen_string_literal: true
 
 class Users::Registrations::PasswordsController < Devise::PasswordsController
+  def create
+    self.resource = if params[:user][:email].present?
+                      resource_class.find_by(email: params[:user][:email])
+                    elsif params[:user][:phone_number].present?
+                      resource_class.find_by(phone_number: params[:user][:phone_number])
+                    else
+                      nil
+                    end
+
+    if resource.present?
+      resource.send_reset_password_instructions
+      yield resource if block_given?
+
+      if successfully_sent?(resource)
+        respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+      else
+        respond_with(resource)
+      end
+    else
+      flash[:alert] = 'Email or phone number not found'
+      respond_with(resource, location: new_password_path(resource_name))
+    end
+  end
+
+  protected
+
+  def resource_params
+    params.require(resource_name).permit(:email, :phone_number)
+  end
   # GET /resource/password/new
   # def new
   #   super
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_624_145_424) do
-  create_table 'user_particulars', force: :cascade do |t|
-    t.string 'full_name'
-    t.string 'phone_number'
-    t.string 'secondary_phone_number'
-    t.string 'country_of_origin'
-    t.string 'ethnicity'
-    t.string 'religion'
-    t.string 'gender'
-    t.string 'date_of_birth'
-    t.string 'date_of_arrival'
-    t.string 'photo_url'
-    t.string 'birth_certificate_url'
-    t.string 'passport_url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
-    t.index ['user_id'], name: 'index_user_particulars_on_user_id'
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_145424) do
+  create_table "user_particulars", force: :cascade do |t|
+    t.string "full_name"
+    t.string "phone_number"
+    t.string "secondary_phone_number"
+    t.string "country_of_origin"
+    t.string "ethnicity"
+    t.string "religion"
+    t.string "gender"
+    t.date "date_of_birth"
+    t.date "date_of_arrival"
+    t.string "photo_url"
+    t.string "birth_certificate_url"
+    t.string "passport_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_user_particulars_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.string 'phone_number', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_particulars_id'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['phone_number'], name: 'index_users_on_phone_number', unique: true
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key 'user_particulars', 'users'
+  add_foreign_key "user_particulars", "users"
 end
