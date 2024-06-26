8819b279f59676465f656074c63a2e051ab5ab59
CYRAHAHAHA <iamacyr@gmail.com>
2024-06-27 03:28:20 +0800

    Error and validation for registering, editing forget password with toggleable phone/email option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def self.find_by_email_or_phone(email_or_phone)
+    where('email = :value OR phone_number = :value', value: email_or_phone).first
+  end
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, format: { with: /\A[0-9]+\z/, message: :invalid }, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
   root 'user_particulars#home' # set home path of web app as /user_particulars/home for now
 
   devise_for :users, controllers: {
-  registrations: 'users/registrations/registrations'
-}
+    registrations: 'users/registrations/registrations',
+    passwords: 'users/registrations/passwords'
+  }
 
   # Self-declared extra routes (must be before resources)
   get 'user_particulars/confirm'
