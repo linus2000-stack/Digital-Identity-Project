1973bc153e0882f35f7a72e703b9e6798180e420
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-04 02:12:33 +0800

    Checking on backend branch

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,5 +65,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.assets.compile = false
+  # added after recommendation from co-pilot yirong
+  config.assets.compile = true
 end
