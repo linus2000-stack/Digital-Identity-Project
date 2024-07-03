94e4ea0b8080d96b9797f97baa5cd0156ba378cd
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-03 14:32:23 +0800

    Separate fawziyah feature from show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! # This will ensure that a user is logged in before accessing any page
-
   helper_method :current_user, :user_signed_in?
   layout :layout_by_resource
-
   def layout_by_resource
     user_signed_in? ? 'application' : 'userlogin'
   end
