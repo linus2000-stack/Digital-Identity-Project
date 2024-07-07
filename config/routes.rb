9545f44af8bd273ebae4cfe1235fd7edb8da2628
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-07 15:06:03 +0800

    Revert "Debugging BDD for login.feature"
    This reverts commit c7b5def2a3a86104ee56312b3ddd05ffd806ab8b.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     registrations: 'users/registrations/registrations',
-    sessions: 'users/registrations/sessions', # Update sessions controller
     passwords: 'users/registrations/passwords'
   }
 
@@ -18,8 +17,7 @@ Rails.application.routes.draw do
   resources :ngo_users
 
   # Custom routes for login, home, and reset password
-  get 'login', to: 'users/registrations/sessions#new', as: 'login'
+  get 'login', to: 'sessions#new', as: 'login'
   get 'home', to: 'home#index', as: 'home'
   get 'reset_password', to: 'password_resets#new', as: 'reset_password'
-  post 'reset_password', to: 'password_resets#create' # Add post route for reset password
 end
