a32038174f9707d9907c726a027cc83ae0e3af97
fawziyah01 <1007214@mymail.sutd.edu.sg>
2024-07-07 22:12:21 +0800

    edited ngo_users new.html.erb and added imageassets

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_707_083_011) do
-  create_table 'ngo_users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.1].define(version: 2024_07_07_083011) do
+  create_table "ngo_users", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'user_particulars', force: :cascade do |t|
-    t.string 'full_name'
-    t.string 'phone_number_country_code'
-    t.string 'phone_number'
-    t.string 'secondary_phone_number_country_code'
-    t.string 'secondary_phone_number'
-    t.string 'country_of_origin'
-    t.string 'ethnicity'
-    t.string 'religion'
-    t.string 'gender'
-    t.date 'date_of_birth'
-    t.date 'date_of_arrival'
-    t.string 'photo_url'
-    t.string 'birth_certificate_url'
-    t.string 'passport_url'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
-    t.string 'full_phone_number'
-    t.string 'full_secondary_phone_number'
-    t.integer 'unique_id', limit: 4
-    t.integer 'two_fa_passcode', limit: 3
-    t.index ['unique_id'], name: 'index_user_particulars_on_unique_id', unique: true
-    t.index ['user_id'], name: 'index_user_particulars_on_user_id'
+  create_table "user_particulars", force: :cascade do |t|
+    t.string "full_name"
+    t.string "phone_number"
+    t.string "secondary_phone_number"
+    t.string "country_of_origin"
+    t.string "ethnicity"
+    t.string "religion"
+    t.string "gender"
+    t.date "date_of_birth"
+    t.date "date_of_arrival"
+    t.string "photo_url"
+    t.string "birth_certificate_url"
+    t.string "passport_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.string "full_phone_number"
+    t.string "full_secondary_phone_number"
+    t.integer "unique_id", limit: 4
+    t.integer "two_fa_passcode", limit: 3
+    t.index ["unique_id"], name: "index_user_particulars_on_unique_id", unique: true
+    t.index ["user_id"], name: "index_user_particulars_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username', default: '', null: false
-    t.string 'email', default: '', null: false
-    t.string 'phone_number', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['phone_number'], name: 'index_users_on_phone_number', unique: true
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key 'user_particulars', 'users'
+  add_foreign_key "user_particulars", "users"
 end
