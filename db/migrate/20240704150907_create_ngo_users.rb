e0aca99eec0139c3329c19115858cc16db646933
fawziyah01 <1007214@mymail.sutd.edu.sg>
2024-07-08 00:35:09 +0800

    edited seed.rb to add ngo_users and _create_ngo_users.rb
    added assets images and

--- db/migrate/20240704150907_create_ngo_users.rb
+++ db/migrate/20240704150907_create_ngo_users.rb
@@ -2,7 +2,7 @@ class CreateNgoUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :ngo_users do |t|
       t.string :name
-
+      t.string :image_url
       t.timestamps
     end
   end
