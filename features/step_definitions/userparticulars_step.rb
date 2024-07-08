e9bcfcaa2653ead52ea165ec41aaf57a3807b50b
Linus <linus_koh@mymail.sutd.edu.sg>
2024-07-08 17:24:41 +0800

    Additional steps for ngo_verify_user.feature

--- features/step_definitions/userparticulars_step.rb
+++ features/step_definitions/userparticulars_step.rb
@@ -107,3 +107,104 @@ Given('I am a logged-in user') do
 end
 
 # From here onwards User Story 2
+# Additional steps for the new feature file
+# Step to navigate to the login page
+Given(/^I am on the “Login” page$/) do
+  visit path_to('login')
+end
+
+# Step to click the "I am a NGO" button
+When(/^I press the "I am a NGO" button$/) do
+  click_button 'I am a NGO'
+end
+
+# Step to check for NGO buttons on the page
+Then(/^I should see a set of different NGO buttons$/) do
+  expect(page).to have_selector(:link_or_button, 'NGO Gebirah')
+end
+
+# Step to click the "NGO Gebirah" button
+When(/^I press the "NGO Gebirah" button$/) do
+  click_button 'NGO Gebirah'
+end
+
+# Step to check for redirection to the NGO: Gebirah page
+Then(/^I should be redirected to the "NGO: Gebirah" page$/) do
+  expect(current_path).to eq(path_to('ngogebirah'))
+end
+
+# Step to check for a welcome message on the NGO: Gebirah page
+And(/^I should see "Welcome Gebirah!"$/) do
+  expect(page).to have_content('Welcome Gebirah!')
+end
+
+# Step to navigate to the NGO: Gebirah page
+Given(/^I am already on my "NGO: Gebirah" page$/) do
+  visit path_to('ngogebirah')
+end
+
+# Step to fill in the EnableID number field
+When(/^I key in the undocumented user's unique EnableID number: (\d+)$/) do |enableid|
+  fill_in 'EnableID number', with: enableid
+end
+
+# Step to fill in the 6 digit code field
+And(/^I key in a 6 digit code that is seen on his/her EnableID: (\d+)$/) do |code|
+  fill_in '6 digit code', with: code
+end
+
+# Step to click the Enter button
+And(/^I press "Enter"$/) do
+  click_button 'Enter'
+end
+
+# Step to check for redirection to the User Verification page
+Then(/^I should be redirected to the "User Verification" page$/) do
+  expect(current_path).to eq(path_to('user verification'))
+end
+
+# Step to check for the EnableID Card on the User Verification page
+And(/^I should see his/her "EnableID Card"$/) do
+  expect(page).to have_content('EnableID Card')
+end
+
+# Step to check for verification guidelines on the User Verification page
+And(/^I should see a set of guidelines to properly verify an EnableID user$/) do
+  expect(page).to have_content('guidelines to properly verify an EnableID user')
+end
+
+# Step to check for the Verify button on the User Verification page
+And(/^I should see a "Verified" Button$/) do
+  expect(page).to have_selector(:link_or_button, 'Verify')
+end
+
+# Step to click the Verify button
+When(/^I press the "Verify" button$/) do
+  click_button 'Verify'
+end
+
+# Step to check for a success message after verification
+Then(/^I should see "Successfully verified EnableID: (\d+)!"$/) do |enableid|
+  expect(page).to have_content("Successfully verified EnableID: #{enableid}!")
+end
+
+# Step to log in as a specific user by EnableID
+Given(/^that I am logged into user (\d+) EnableID account$/) do |enableid|
+  # Assuming there's a method to log in as a specific user by their EnableID
+  log_in_as_enableid_user(enableid)
+end
+
+# Step to check for a verified checkmark on the EnableID card
+Then(/^I should see the checkmark on the user's EnableID card$/) do
+  expect(page).to have_selector('.verified-checkmark')
+end
+
+# Step to check for verification message on the EnableID card
+And(/^I should see "EnableID - verified by NGO: Gebirah"$/) do
+  expect(page).to have_content('EnableID - verified by NGO: Gebirah')
+end
+
+# Step to check for the date of verification on the EnableID card
+And(/^I should see "Date of verification: (today's date)"$/) do
+  expect(page).to have_content("Date of verification: #{Date.today.strftime('%Y-%m-%d')}")
+end
