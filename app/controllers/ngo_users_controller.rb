9e1faaae80de0327c988d3772aea527d57de0b36
Brendan <gacky.robert@gmail.com>
2024-07-10 18:56:09 +0800

    updated unique id and 2fa page for show.html to now show both fields at the start and flash necessary errors

--- app/controllers/ngo_users_controller.rb
+++ app/controllers/ngo_users_controller.rb
@@ -2,20 +2,10 @@ class NgoUsersController < ApplicationController
   def new
   end
 
-  def show
-    
+  def show 
     @ngo_user = NgoUser.find(params[:id])
     return unless params[:unique_id].present?
 
-    @user_particular = UserParticular.find_by(unique_id: params[:unique_id])
-    if @user_particular
-      flash.now[:notice] = 'Unique ID found. Please enter your 2FA code.'
-      @unique_id_exists = true
-    else
-      flash[:alert] = 'Unique ID does not exist. Please try again.'
-      @unique_id_exists = false
-    end
-    @back_path = ngo_users_path
   end
 
   def index
@@ -31,18 +21,11 @@ class NgoUsersController < ApplicationController
   def check_user
     @ngo_user = NgoUser.find(params[:id])
     @user_particular = UserParticular.find_by(unique_id: params[:unique_id], two_fa_passcode: params[:two_fa_passcode])
-    logger.debug "User Particular: #{@user_particular}"
     if @user_particular
-      flash.now[:notice] = 'User found.'
-      @checked = true
-      @unique_id_exists = true
-      params[:unique_id] = @user_particular.unique_id
-      logger.debug "params[:unique_id]: #{params[:unique_id]}"
-      redirect_to verify_ngo_user_path(@ngo_user, unique_id: @user_particular.unique_id) # Yiqing redirect to verify code
+      redirect_to verify_ngo_user_path(@ngo_user, checked: true, unique_id: @user_particular.unique_id)
     else
-      flash[:alert] = 'User not found.'
-      @checked = false
-      @unique_id_exists = true
+      flash[:alert] = 'User not found. Please check your Unique ID and 2FA Code.'
+      render :show
     end
   end
 
