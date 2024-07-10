162ab8b3316a5837e2985c52d78f5ff63b9f4abe
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-11 03:40:17 +0800

    Adding env to use login_as for bdd

--- features/step_definitions/rapidtest_step.rb
+++ features/step_definitions/rapidtest_step.rb
@@ -87,9 +87,7 @@ Given(/^I have a user (\d+) verified by NGO: (.+), and logs in$/) do |user_id, n
   @user_particular = UserParticular.find_by(unique_id: user_id)
   @user = User.find_by(id: @user_particular.user_id)
   step 'I am on the "Login" page'
-  fill_in 'Log in', with: @user.username
-  fill_in 'Password', with: '$2a$04$Vq4zF.IhWYCHqSuL2gOumepveZ1E38fXgW.N7kY.rgFiqPK6xkJ8O'
-  click_button 'Log in'
+  login_as(@user, scope: :user)
   expect(page).to have_content('Welcome,')
 end
 
