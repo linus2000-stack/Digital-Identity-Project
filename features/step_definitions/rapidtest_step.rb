da329a89edad4085361c8b1bba774ac26c9c974b
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-10 23:55:09 +0800

    BDD, last scenario for user story 2

--- features/step_definitions/rapidtest_step.rb
+++ features/step_definitions/rapidtest_step.rb
@@ -1,6 +1,30 @@
+Given('a user particular exists with unique ID {string} and 2FA code {string}') do |unique_id, two_fa_code|
+  UserParticular.find_by_id(1).update(unique_id:, two_fa_passcode: two_fa_code)
+  $user_particular = UserParticular.find_by_id(1)
+end
+
+Given('I am on "User Verification" page') do
+  # Assuming the journey starts at the "Home" page and requires login
+  step 'I am already on my NGO "Gebirah" page'
+
+  step 'I key in the undocumented user\'s unique EnableID number: 1071783'
+  step 'I key in a 6 digit code that is seen on his/her EnableID: 958523'
+  step 'I press "Check" button'
+end
+
 # Step to click a specific button
 And(/^I press the "([^"]*)" button$/) do |button|
-  click_link button
+  if has_link?(button)
+    click_link button
+  elsif has_button?(button)
+    click_button button
+  else
+    find("input[type='submit'][value='#{button.capitalize}']").click
+  end
+end
+
+And(/^I press "Check" button$/) do
+  click_button 'Check'
 end
 
 # Step to navigate to a specific page
@@ -18,22 +42,16 @@ When(/^I press the NGO "([^"]*)" card$/) do |image_alt|
   card_link.click
 end
 
-Then(/^I should be redirected to the "([^"]*)" page$/) do |page_name|
-  expected_path = if page_name == 'User Verification'
-                    verify_ngo_user_path(@ngo_user, unique_id: @user_particular.unique_id)
-                  else
-                    "/ngo_users/#{ngo_user.id}"
-                  end
-
-  expect(current_path).to eq(expected_path),
-                          "Expected to be redirected to #{expected_path} but was at #{current_path} instead."
+Then('I should be redirected to the "User Verification" page and see "EnableID Verification"') do
+  puts current_path
+  step 'I should see "EnableID Verification"'
 end
 
 Then(/^I should be redirected to the NGO "([^"]*)" page$/) do |page_name|
-  ngo_user = NgoUser.find_by(name: page_name)
-  expect(ngo_user).not_to be_nil, "NgoUser with name #{page_name} not found."
+  $ngo_user = NgoUser.find_by(name: page_name)
+  expect($ngo_user).not_to be_nil, "NgoUser with name #{page_name} not found."
 
-  expected_path = "/ngo_users/#{ngo_user.id}"
+  expected_path = "/ngo_users/#{$ngo_user.id}"
   expect(current_path).to eq(expected_path),
                           "Expected to be redirected to #{expected_path} but was at #{current_path} instead."
 end
@@ -44,9 +62,9 @@ end
 
 Given(/^I am already on my NGO "([^"]*)" page$/) do |ngo_name|
   step 'I am on the "Login" page'
-  puts "Current path: |||||||| #{current_path} ||||||||"
   step 'I press the "I am a NGO" button'
   step %(I press the NGO "#{ngo_name}" card)
+  $ngo_user = NgoUser.find_by(name: ngo_name)
 end
 
 When(/^I key in the undocumented user's unique EnableID number: (\d+)$/) do |enable_id|
@@ -57,14 +75,13 @@ And(%r{^I key in a 6 digit code that is seen on his/her EnableID: (\d{6})$}) do 
   fill_in 'two_fa_passcode', with: code
 end
 
-Then('I should see his/her EnableID card') do
+Then('I should see his\/her EnableID card') do
   expect(page).to have_selector('#digitalcard')
 end
 
 Then(/^I should see "([^"]*)" button$/) do |button_text|
-  expect(page).to have_selector('#ngo-verify-btn')
+  expect(page).to have_selector(".btn[value='#{button_text}']")
 end
-
 # Maps page names to their corresponding paths
 def path_to(page_name)
   case page_name.downcase
