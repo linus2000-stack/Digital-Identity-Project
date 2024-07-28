a7dd6edb7bc6ebc28fd14d0ecc853af55e60f51c
CYRAHAHAHA <iamacyr@gmail.com>
2024-07-29 01:40:58 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_28_083353) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_28_114518) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_28_083353) do
     t.boolean "saved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "ngo_users_id", null: false
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ngo_users_id"], name: "index_messages_on_ngo_users_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "ngo_users", force: :cascade do |t|
@@ -115,6 +125,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_28_083353) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "ngo_users", column: "ngo_users_id"
+  add_foreign_key "messages", "users"
   add_foreign_key "user_histories", "users"
   add_foreign_key "user_particulars", "users"
 end
