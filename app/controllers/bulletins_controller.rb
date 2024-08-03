fa32230ce1c318c209c4f0fb0be0323414625917
Brendan <gacky.robert@gmail.com>
2024-08-03 17:43:54 +0800

    upload photo for bulletin post is working but still need to work on default ngo image if no photo is uploaded

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -11,6 +11,21 @@ class BulletinsController < ApplicationController
     logger.debug "User ID: #{params[:user_id]}"
     @ngo_user = NgoUser.find(params[:bulletin][:user_id])
     @bulletin = Bulletin.new(bulletin_params)
+    uploaded_io = params[:bulletin][:photo]
+
+    uploads_dir = Rails.root.join('public', 'uploads')
+    FileUtils.mkdir_p(uploads_dir) unless File.directory?(uploads_dir)
+
+    if uploaded_io.present?
+      file_path = uploads_dir.join(uploaded_io.original_filename)
+      File.open(file_path, 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+      @bulletin.photo = uploaded_io.original_filename
+    else
+      @bulletin.photo = @ngo_user.image_url # Use the NGO image URL if no file is uploaded
+    end
+    
     if @bulletin.save
       flash[:success] = "Post added successfully."
       redirect_to ngo_user_path(@ngo_user.id) # Adjust the redirect path as needed
@@ -23,6 +38,6 @@ class BulletinsController < ApplicationController
   private
 
   def bulletin_params
-    params.require(:bulletin).permit(:title, :description, :date, :location, :ngo_name)
+    params.require(:bulletin).permit(:title, :description, :date, :location, :ngo_name, :photo)
   end
 end
--- features/ngo_add_post.feature
+++ features/ngo_add_post.feature
@@ -41,3 +41,15 @@ Scenario: Missing fields when adding posts
   | Date                      | 31-1-2024         |
   | Location                  | Malaysia          |
   Then I should see a validation message "Please fill out this field."
+
+Scenario: Add photo to event post
+  When I press the "Add Bulletin Post" button
+  Given I fill in the following fields
+  | Field                     | Value             |
+  | Title                     | Event 1           |
+  | Description               | Food Distribution |
+  | Date                      | 31-1-2024         |
+  | Location                  | Malaysia          |
+  And I press the "Upload a photo here" button
+  When I press the "Add Post" button
+  Then I should see event card with the uploaded photo
