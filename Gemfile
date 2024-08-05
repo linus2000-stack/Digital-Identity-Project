d75380b29f55a55649657294e6d71f08a21c2074
Linus <linus_koh@mymail.sutd.edu.sg>
2024-08-06 02:26:47 +0800

    save uploaded documents

--- Gemfile
+++ Gemfile
@@ -21,8 +21,6 @@ gem 'puma', '>= 5.0'
 gem 'prawn'
 # Use CarrierWave
 gem 'carrierwave', '~> 2.0'
-# Use ActiveStorage
-gem 'activestorage'
 
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem 'importmap-rails'
--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -1,12 +1,37 @@
 class UploadedFilesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user_particular
+
+  def index
+    @uploaded_files = @user_particular.uploaded_files
+    render json: @uploaded_files
+  end
 
   def create
     @uploaded_file = @user_particular.uploaded_files.build(uploaded_file_params)
     @uploaded_file.status = 'Unverified' if @uploaded_file.status.blank?
+    @uploaded_file.user_id = current_user.id
 
     if @uploaded_file.save
       render json: { success: true, file: @uploaded_file.as_json.merge({ file_url: @uploaded_file.file_url }) }, status: :created
+    else
+      render json: { success: false, errors: @uploaded_file.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue => e
+    logger.error "Failed to upload file: #{e.message}"
+    render json: { success: false, errors: ["Failed to upload file"] }, status: :internal_server_error
+  end
+
+  def destroy
+    @uploaded_file = @user_particular.uploaded_files.find(params[:id])
+    @uploaded_file.destroy
+    head :no_content
+  end
+
+  def update
+    @uploaded_file = @user_particular.uploaded_files.find(params[:id])
+    if @uploaded_file.update(uploaded_file_params)
+      render json: { success: true, file: @uploaded_file }
     else
       render json: { success: false, errors: @uploaded_file.errors.full_messages }, status: :unprocessable_entity
     end
