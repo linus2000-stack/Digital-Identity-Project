c24fdcfb27c42b7e16b8c52fc250e70823a25637
teohbenji <teohbenjamin1@gmail.com>
2024-08-07 01:18:58 +0800

    update user spec and rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ SimpleCov.start 'rails' do
   add_filter 'app/controllers/users'
   add_filter 'app/jobs'
   add_filter 'app/mailers'
-  add_filter 'app/helpers/user_particulars_helper.rb'
+  add_filter 'app/helpers'
 end
 
 ENV['RAILS_ENV'] ||= 'test'
