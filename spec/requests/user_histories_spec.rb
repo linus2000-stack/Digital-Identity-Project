1ac62432c021ea87a0eba43d4f10f5d94a15f56d
teohbenji <teohbenjamin1@gmail.com>
2024-08-07 01:24:07 +0800

    silence all pending tests

--- spec/requests/user_histories_spec.rb
+++ spec/requests/user_histories_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "UserHistories", type: :request do
   describe "GET /create" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/user_histories/create"
       expect(response).to have_http_status(:success)
     end
